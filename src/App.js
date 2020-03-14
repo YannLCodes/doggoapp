947bcf9abf3d6e8b7f6d6b31344450a8bb83d383
YannLCodes <YannLCodes@hotmail.com>
2020-03-14 20:03:51 +0000

    Added samoyed to search

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
 
   return (
     <div className="App">
-      Hello
 	  	<UsApi />
     </div>
   );
--- src/services/UsApi.js
+++ src/services/UsApi.js
@@ -5,25 +5,24 @@ export const UsApi = () => {
 	const apik = "Qe5ddre98VQQC8vLpY-fXCVGkwOCmDr-eVKZbEevlDQ";
 	const apiUrl = "https://api.unsplash.com/photos/";
 	const randomUrl = "random/";
+	const shoobQuery = "query=bernese"
 	const [item, setItem] = useState(null);
 	const [loading, setLoading] = useState(true);
 
 
 	useEffect(async()=>{ 
-		const call = await fetch(`${apiUrl}${randomUrl}?client_id=${apik}`); //Make the API Call
+		const call = await fetch(`${apiUrl}${randomUrl}?client_id=${apik}&${shoobQuery}`); //Make the API Call
 		const data = await call.json();
 		const foo = data.urls.regular;
 		setItem(foo);
 		setLoading(false);
 		console.log(data);
 	}, [])
-	
-
 
 	
 	return(
 		<>
-		{loading ? <h3> ... Loading ... </h3> : <img src={item} /> }
+		{loading ? <h3> ... Loading ... </h3> : <img class="image-generated" src={item} /> }
 		</>
 	)
 }
